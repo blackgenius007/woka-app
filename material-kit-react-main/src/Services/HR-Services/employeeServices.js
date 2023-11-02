6d6c256ed30684d3eef7b3df6b9fec728024148f
blackgenius007 <truthin3dimension@gmail.com>
2023-11-02 12:48:23 +0100

    added vercel config to root

--- material-kit-react-main/src/Services/HR-Services/employeeServices.js
+++ material-kit-react-main/src/Services/HR-Services/employeeServices.js
@@ -24,7 +24,7 @@ const API_URL_FETCH_ALL_ATTENDANCE  = '/api/v1/employee/all-attendance'
 const register = async (employeeData, employeeId) => {
   console.log('employee-service =>', employeeData);
   const response = await axios.post(
-    `${API_URL_REGISTER}/${employeeId}`,
+    `${BASE_URL+API_URL_REGISTER}/${employeeId}`,
     employeeData
   );
   return response.data;
@@ -33,14 +33,14 @@ const register = async (employeeData, employeeId) => {
 // Retrieve all employees
 const retrieveEmployees = async (email) => {
   console.log('retrievedEmployee=>', email);
-  const response = await axios.get(`${API_URL_RETRIEVE}/${email}`);
+  const response = await axios.get(`${BASE_URL+API_URL_RETRIEVE}/${email}`);
   console.log('employees retrieved =>', response);
   return response.data;
 };
 
 // Retrieve employee designations
 const retrieveDesignations = async (email, projectname) => {
-  const response = await axios.get(`${API_URL_RETRIEVE_DESIGNATION}/${email}`, {
+  const response = await axios.get(`${BASE_URL+API_URL_RETRIEVE_DESIGNATION}/${email}`, {
     params: {
       _limit: 100,
     },
@@ -50,7 +50,7 @@ const retrieveDesignations = async (email, projectname) => {
 
 // Retrieve a single employee
 const retrieveEmployee = async (employeeNum) => {
-  const response = await axios.get(`${API_URL_DETAIL}/${employeeNum}`);
+  const response = await axios.get(`${BASE_URL+API_URL_DETAIL}/${employeeNum}`);
   console.log('single mployees retrieved =>', response);
   return response.data;
 };
@@ -58,21 +58,21 @@ const retrieveEmployee = async (employeeNum) => {
 // Update a single employee
 const updateEmployee = async (id, formData) => {
   console.log('update-id', id);
-  const response = await axios.post(`${API_URL_UPDATE}/${id}`, formData);
+  const response = await axios.post(`${BASE_URL+API_URL_UPDATE}/${id}`, formData);
   console.log(response);
   return response.data;
 };
 
 // Delete a single employee
 const deleteEmployee = async (id) => {
-  const response = await axios.delete(`${API_URL_DELETE}/${id}`);
+  const response = await axios.delete(`${BASE_URL+API_URL_DELETE}/${id}`);
   return response.data;
 };
 
 // Save payroll data for an employee
 const savePayrollData = async (requestPayload) => {
   console.log('employeeId=>', requestPayload);
-  const response = await axios.post(`${API_PAYROLL}`,requestPayload );
+  const response = await axios.post(`${BASE_URL+API_PAYROLL}`,requestPayload );
   return {
       requestPayload: response.data,
   };
@@ -80,14 +80,14 @@ const savePayrollData = async (requestPayload) => {
 
 // Complain status
 const  complainStatus= async (date, employeeId, complainDetail, dueDate,label ) => {
-  const response = await axios.get(`${API_URL_COMPLAIN_STATUS}/${employeeId}/${dueDate}/${date}/${label}/${complainDetail}`);
+  const response = await axios.get(`${BASE_URL+API_URL_COMPLAIN_STATUS}/${employeeId}/${dueDate}/${date}/${label}/${complainDetail}`);
   return  response.data;
 };
 
 // Update complain
 const updateComplain = async (date) => {
   console.log('updateComplain date=>',date)
-  const response = await axios.post(`${API_URL_UPDATE_COMPLAIN}/${date}`);
+  const response = await axios.post(`${BASE_URL+API_URL_UPDATE_COMPLAIN}/${date}`);
 return response.data;
 
 };
@@ -95,14 +95,14 @@ return response.data;
 // Mark today for a single employee
 const markTodayEmployee = async (date,employeeId,label) => {
   console.log('employeeId:',employeeId,'label:',label,'date:',date)
-  const response = await axios.get(`${API_URL_MARK_TODAY}/${date}/${employeeId}/${label}`);
+  const response = await axios.get(`${BASE_URL+API_URL_MARK_TODAY}/${date}/${employeeId}/${label}`);
   return response.data;
 };   
 
 // Mark today for all employees
 const markTodayAllEmployees = async (userEmail, label, date) => {
   console.log(userEmail, label, date)
-  const response = await axios.get(`${API_URL_MARK_ALL_TODAY}/${userEmail}/${date}/${label}`);
+  const response = await axios.get(`${BASE_URL+API_URL_MARK_ALL_TODAY}/${userEmail}/${date}/${label}`);
   console.log(response)
   // return response.data;
  
@@ -110,7 +110,7 @@ const markTodayAllEmployees = async (userEmail, label, date) => {
 
 // Retrieve all employee attendance
 const retrieveAllAttendance = async (userEmail, dateOffset) => {
-  const response = await axios.get(`${API_URL_FETCH_ALL_ATTENDANCE}/${userEmail}/${dateOffset}`);
+  const response = await axios.get(`${BASE_URL+API_URL_FETCH_ALL_ATTENDANCE}/${userEmail}/${dateOffset}`);
   return response.data;
 };
 
