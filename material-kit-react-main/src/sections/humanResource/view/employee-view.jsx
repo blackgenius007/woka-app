77a349f60a5aad92c71fb0663bd1061fe1a29d7e
blackgenius007 <truthin3dimension@gmail.com>
2023-11-04 15:46:50 +0100

    added employee component

--- material-kit-react-main/src/sections/humanResource/view/employee-view.jsx
+++ material-kit-react-main/src/sections/humanResource/view/employee-view.jsx
@@ -1,5 +1,5 @@
    /* eslint-disable */
-   import React, { useState } from 'react';
+   import React, { useState,useEffect } from 'react';
    import { useDispatch, useSelector } from 'react-redux';
    import Button from '@mui/material/Button';
    import Container from '@mui/material/Container';
