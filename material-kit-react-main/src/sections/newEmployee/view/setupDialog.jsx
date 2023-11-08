16bc693844f87ccfd9d2db1fa28a6f55e989f7f7
blackgenius007 <truthin3dimension@gmail.com>
2023-11-08 22:52:06 +0100

    payroll

--- material-kit-react-main/src/sections/newEmployee/view/setupDialog.jsx
+++ material-kit-react-main/src/sections/newEmployee/view/setupDialog.jsx
@@ -1,19 +1,21 @@
 /* eslint-disable */
 import * as React from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import ListItemText from '@mui/material/ListItemText';
-import ListItem from '@mui/material/ListItem';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import AppBar from '@mui/material/AppBar';
-import Stack from '@mui/material/Stack';
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  Grid,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Slide,
+  Stack,
+  Typography,
+ 
+} from '@mui/material';
+ 
 import Iconify from 'src/components/iconify';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import CloseIcon from '@mui/icons-material/Close';
-import Slide from '@mui/material/Slide';
 import PayrollSettings from './payrollSetting';
 import Department from './department';
 
