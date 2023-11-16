f06a9c6a60a0f2e1f9eb65c782c2148f8c19d100
blackgenius007 <truthin3dimension@gmail.com>
2023-11-16 06:03:57 +0100

    Added error boundary

--- material-kit-react-main/src/ErrorBoundary.jsx
+++ material-kit-react-main/src/ErrorBoundary.jsx
@@ -1,33 +1,38 @@
    /* eslint-disable */
-import React from 'react';
-import PropTypes from 'prop-types';
-
-
-class ErrorBoundary extends React.Component {
-  static propTypes = {
-    children: PropTypes.node.isRequired,
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false };
-  }
-
-  componentDidCatch(error, errorInfo) {
-    // You can log the error to an error reporting service
-    // For example, you can use a service like Sentry or log to the console
-    console.error('Error caught by ErrorBoundary:', error, errorInfo);
-    this.setState({ hasError: true });
-  }
-
-  render() {
-    if (this.state.hasError) {
-      // You can render a fallback UI here
-      return <div>Something went wrong. Please try again later.</div>;
-    }
-
-    return this.props.children;
-  }
-}
-
-export default ErrorBoundary;
+   import React from 'react';
+   import PropTypes from 'prop-types';
+   
+   class ErrorBoundary extends React.Component {
+     static propTypes = {
+       children: PropTypes.node.isRequired,
+     };
+   
+     constructor(props) {
+       super(props);
+       this.state = { hasError: false };
+     }
+   
+     componentDidCatch(error, errorInfo) {
+       // You can log the error to an error reporting service
+       // For example, you can use a service like Sentry or log to the console
+       console.error('Error caught by ErrorBoundary:', error, errorInfo);
+       this.setState({ hasError: true });
+   
+       // Go back in history
+       window.history.back();
+     }
+   
+     render() {
+       const { children } = this.props;
+   
+       if (this.state.hasError) {
+         // You can render a fallback UI here
+         return <div>Something went wrong. Please try again later.</div>;
+       }
+   
+       return children;
+     }
+   }
+   
+   export default ErrorBoundary;
+   
