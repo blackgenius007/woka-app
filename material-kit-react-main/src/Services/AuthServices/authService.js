0c2d883ec98d4b71d94a7cdbae92cc9126afa0a9
blackgenius007 <truthin3dimension@gmail.com>
2023-11-16 12:06:49 +0100

    modified auth services

--- material-kit-react-main/src/Services/AuthServices/authService.js
+++ material-kit-react-main/src/Services/AuthServices/authService.js
@@ -1,72 +1,70 @@
-   /* eslint-disable */
+ /* eslint-disable */
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 // Define your base URL here
- 
 const BASE_URL = 'https://woka-app.vercel.app';
 
 axios.defaults.baseURL = BASE_URL;
 
+const API_URL_REGISTER = '/api/v1/auth/register';
+const API_URL_LOGIN = '/api/v1/auth/login';
+const API_URL_USERS_BY_ID = '/api/v1/auth/users';
+const API_URL_DEPARTMENT = '/api/v1/auth/department';
 
-const API_URL_R = '/api/v1/auth/register'
-const API_URL_L = '/api/v1/auth/login'
-const API_URL_USER_BY_ID = '/api/v1/auth/users'
-const API_URL_DEPARTMENT = '/api/v1/auth/department'
-
+// Function to construct the base URL
+const constructURL = (endpoint) => `${BASE_URL}${endpoint}`;
 
 // Register user
-const register = async (RegistrationData,navigate) => {
-  console.log('auth-service =>',RegistrationData)
-  const response = await axios.post(BASE_URL+API_URL_R,RegistrationData)
+const register = async (registrationData, navigate) => {
+  console.log('auth-service =>', registrationData);
+  const response = await axios.post(constructURL(API_URL_REGISTER), registrationData);
 
   if (response.data) {
-    localStorage.setItem('user', JSON.stringify(response.data))
+    localStorage.setItem('user', JSON.stringify(response.data));
     navigate('/welcome'); // Redirect to welcome route
   }
 
-  return response.data
-}
+  return response.data;
+};
 
 // Login user
 const login = async (userData) => {
-  const response = await axios.post(BASE_URL+API_URL_L , userData)
+  const response = await axios.post(constructURL(API_URL_LOGIN), userData);
 
   if (response.data) {
-    localStorage.setItem('user', JSON.stringify(response.data))
-    }
+    localStorage.setItem('user', JSON.stringify(response.data));
+  }
 
-  return response.data
-}
+  return response.data;
+};
 
 // Logout user
 const logout = (navigate) => {
   localStorage.removeItem('user');
- navigate('/'); // Redirect to home route
+  navigate('/'); // Redirect to home route
 };
 
-// Retrieve allUser  inmate
+// Retrieve user by ID
 const retrieveUsers = async (id) => {
-  const response = await axios.get(`${API_URL_USER_BY_ID}/${id}`);
+  const response = await axios.get(constructURL(`${API_URL_USERS_BY_ID}/${id}`));
   return response.data;
 };
 
+// Save department data
 const saveDepartment = async (email, department) => {
   const formData = new FormData();
   department.forEach((user) => formData.append('department', JSON.stringify(user)));
-  const response = await axios.post(`${API_URL_DEPARTMENT}/${email}`, formData);
+  const response = await axios.post(constructURL(`${API_URL_DEPARTMENT}/${email}`), formData);
   return response.data;
 };
+
 const authService = {
   register,
   logout,
   login,
   retrieveUsers,
   saveDepartment,
-}
+};
 
-export default authService
-
-
-
- 
+export default authService;
