27bebb5e345b319caa5ebbfef7dcdb827defcd71
blackgenius007 <truthin3dimension@gmail.com>
2023-11-24 15:52:31 +0100

    Ledger

--- material-kit-react-main/src/sections/employeeDetail/view/ledger.jsx
+++ material-kit-react-main/src/sections/employeeDetail/view/ledger.jsx
@@ -107,7 +107,7 @@ const Ledger = ({ healthCare, grossIncome, employeeId, country }) => {
   // Retrieve financial data from financialSlice
   const financialData = useSelector((state) => state.financial[employeeId]);
 
-  console.log('financial-data :', financialData.monthlySalary );
+  console.log('financial-data :', financialData  && financialData.monthlySalary );
 
   // Save todos to local storage whenever it changes
   useEffect(() => {
