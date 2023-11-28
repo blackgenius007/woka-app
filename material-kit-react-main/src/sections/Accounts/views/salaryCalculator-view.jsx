cc1eaf9558e6dbe8170a24fe97000fcacafe0315
blackgenius007 <truthin3dimension@gmail.com>
2023-11-28 21:38:53 +0100

    Salary

--- material-kit-react-main/src/sections/Accounts/views/salaryCalculator-view.jsx
+++ material-kit-react-main/src/sections/Accounts/views/salaryCalculator-view.jsx
@@ -1,5 +1,6 @@
  /* eslint-disable */
 import React, { useState, useEffect  } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { retrieveAllEmployees } from 'src/Services/HR-Services/employeeSlice';
 
 import {
