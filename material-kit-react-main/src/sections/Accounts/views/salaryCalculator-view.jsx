11387875e8f46787fe56683cd0a6eda3b19a39f7
blackgenius007 <truthin3dimension@gmail.com>
2023-11-28 09:10:42 +0100

    Salary

--- material-kit-react-main/src/sections/Accounts/views/salaryCalculator-view.jsx
+++ material-kit-react-main/src/sections/Accounts/views/salaryCalculator-view.jsx
@@ -497,50 +497,17 @@ console.log('function for salary=>',totalRemunerationForAll)
           Go back to employee panel
         </Popup> */}
 
-        <Popup
-          trigger={
+ 
             <Button variant="info" color="primary"  >
-              <IconContext.Provider value={{ color: 'grey', size: '25px' }}>
-                <FcOvertime />
-              </IconContext.Provider>
+          
+            <Icon icon="flat-color-icons:overtime" width="45" height="45" />
+             
             </Button>
-          }
-          position="bottom center"
-        >
-          Reset all employee overtime to zero   
-        </Popup>
+       
 
-        <IconButton>
-          <Popup
-            trigger={
-              <Link>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="icon icon-tabler icon-tabler-businessplan"
-                  width="25"
-                  height="25"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="#9e9e9e"
-                  fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <ellipse cx="16" cy="6" rx="5" ry="3" />
-                  <path d="M11 6v4c0 1.657 2.239 3 5 3s5 -1.343 5 -3v-4" />
-                  <path d="M11 10v4c0 1.657 2.239 3 5 3s5 -1.343 5 -3v-4" />
-                  <path d="M11 14v4c0 1.657 2.239 3 5 3s5 -1.343 5 -3v-4" />
-                  <path d="M7 9h-2.5a1.5 1.5 0 0 0 0 3h1a1.5 1.5 0 0 1 0 3h-2.5" />
-                  <path d="M5 15v1m0 -8v1" />
-                </svg>
-              </Link>
-            }
-            position="bottom center"
-          >
-            Reset all employee allowance to zero
-          </Popup>
-        </IconButton>
+        <Button>
+        <Icon icon="arcticons:moneybuster" width="45" height="45" />
+        </Button>
 
        
         <Chip
