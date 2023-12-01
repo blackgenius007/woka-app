2ff4febcc1bd523d515d618f20b81b481050858d
blackgenius007 <truthin3dimension@gmail.com>
2023-12-01 23:11:10 +0100

    Salary

--- material-kit-react-main/src/sections/Accounts/views/salaryCalculator-view.jsx
+++ material-kit-react-main/src/sections/Accounts/views/salaryCalculator-view.jsx
@@ -675,7 +675,7 @@ const SalaryCalculator = ({ drawer }) => {
                 <strong>Total:</strong>
               </td>
               <td style={{ textAlign: 'center' }}>
-                {exportMode ? fNumber(totalRemunerationForAll.toFixed(2)) : '-'}
+                {exportMode ? fNumber(totalRemunerationForAll.toFixed(2)) : fNumber(totalRemunerationForAll.toFixed(2))}
               </td>
             </tr>
           </tbody>
