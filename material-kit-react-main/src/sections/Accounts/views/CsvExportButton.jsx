628ac2b5f523fd6ecdbe8c0ba42375d284dec33a
blackgenius007 <truthin3dimension@gmail.com>
2023-12-03 21:57:32 +0100

    salary

--- material-kit-react-main/src/sections/Accounts/views/CsvExportButton.jsx
+++ material-kit-react-main/src/sections/Accounts/views/CsvExportButton.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { CSVLink } from 'react-csv';
-import Button from '@mui/material/Button';
+// import React from 'react';
+// import { CSVLink } from 'react-csv';
+// import Button from '@mui/material/Button';
 
-const CsvExportButton = ({ data, filename }) => {
-  return (
-    <CSVLink data={data} filename={filename}>
-      <Button>
-        Export to CSV
-      </Button>
-    </CSVLink>
-  );
-};
+// const CsvExportButton = ({ data, filename }) => {
+//   return (
+//     <CSVLink data={data} filename={filename}>
+//       <Button>
+//         Export to CSV
+//       </Button>
+//     </CSVLink>
+//   );
+// };
 
-export default CsvExportButton;
+// export default CsvExportButton;
