fa68ae7a9584f098eb39ec82f1ebd1c290ba20df
blackgenius007 <truthin3dimension@gmail.com>
2023-12-04 09:17:41 +0100

    salary

--- material-kit-react-main/src/sections/Accounts/views/ExportToExcelButton.jsx
+++ material-kit-react-main/src/sections/Accounts/views/ExportToExcelButton.jsx
@@ -1,7 +1,8 @@
- /* eslint-disable */
+/* eslint-disable */
 import React from 'react';
 import { saveAs } from 'file-saver';
-import * as XLSX from 'xlsx';
+import { utils, writeFile } from 'xlsx';
+import Iconify from '@iconify/react';
 import Button from '@mui/material/Button';
 
 const ExportToExcelButton = ({ tableRef }) => {
@@ -9,24 +10,61 @@ const ExportToExcelButton = ({ tableRef }) => {
     const table = tableRef.current;
 
     // Create a worksheet
-    const ws = XLSX.utils.table_to_sheet(table);
+    const ws = utils.table_to_sheet(table);
 
     // Create a workbook
-    const wb = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+    const wb = utils.book_new();
+    utils.book_append_sheet(wb, ws, 'Sheet1');
 
     // Convert the workbook to a data URL
-    const blob = XLSX.write(wb, { bookType: 'xlsx', type: 'blob' });
+    const blob = writeFile(wb, { bookType: 'xlsx', mimeType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
 
     // Save the data URL as a file
     saveAs(blob, 'exported_data.xlsx');
   };
 
   return (
-    <Button onClick={handleExportToExcel}>
-     Export to Excel
+    <Button onClick={() => handleExportToExcel()}>
+      Export to Excel
     </Button>
   );
 };
 
 export default ExportToExcelButton;
+
+
+
+
+
+//  /* eslint-disable */
+// import React from 'react';
+// import { saveAs } from 'file-saver';
+// import * as XLSX from 'xlsx';
+// import Button from '@mui/material/Button';
+
+// const ExportToExcelButton = ({ tableRef }) => {
+//   const handleExportToExcel = () => {
+//     const table = tableRef.current;
+
+//     // Create a worksheet
+//     const ws = XLSX.utils.table_to_sheet(table);
+
+//     // Create a workbook
+//     const wb = XLSX.utils.book_new();
+//     XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+//     // Convert the workbook to a data URL
+//     const blob = XLSX.write(wb, { bookType: 'xlsx', type: 'blob' });
+
+//     // Save the data URL as a file
+//     saveAs(blob, 'exported_data.xlsx');
+//   };
+
+//   return (
+//     <Button onClick={handleExportToExcel}>
+//      Export to Excel
+//     </Button>
+//   );
+// };
+
+// export default ExportToExcelButton;
