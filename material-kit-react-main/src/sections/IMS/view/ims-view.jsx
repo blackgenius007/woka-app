d67141cad31a7d37d5ca5724b67d4ab971b831f9
blackgenius007 <truthin3dimension@gmail.com>
2023-12-08 14:35:36 +0100

    update form

--- material-kit-react-main/src/sections/IMS/view/ims-view.jsx
+++ material-kit-react-main/src/sections/IMS/view/ims-view.jsx
@@ -1,13 +1,12 @@
-/* eslint-disable */
+ /* eslint-disable */
 
 import React from 'react';
-import { Container, Typography, Button, Stack } from '@mui/material';
+import { Container, Typography, Button, Stack, useMediaQuery } from '@mui/material';
 import { Link } from 'react-router-dom';
 import Scrollbar from 'src/components/scrollbar';
 // import InventoryTable from 'components/InventoryTable'; // Adjust this import based on your project structure
 import { Icon } from '@iconify/react';
 import Iconify from 'src/components/iconify';
- 
 
 const IMSPage = () => {
   const handleLowStockAlerts = () => {
@@ -18,9 +17,11 @@ const IMSPage = () => {
     // Add logic for Order Status Updates
   };
 
+  const isSmallScreen = useMediaQuery('(max-width: 600px)');
+
   return (
     <Container>
-      <Stack direction="column" spacing={3} alignItems="center">
+      <Stack direction={isSmallScreen ? 'column' : 'row'} spacing={3} alignItems="center">
         <Typography variant="h4">Inventory Management System</Typography>
 
         {/* Add button for creating a new inventory item */}
@@ -34,7 +35,7 @@ const IMSPage = () => {
         </Link>
 
         {/* Add buttons or icons for key elements */}
-        <Stack direction="column" spacing={2} width="100%">
+        <Stack direction={isSmallScreen ? 'column' : 'row'} spacing={2} width="100%">
           <Button
             variant="contained"
             style={{ backgroundColor: '#00CC66', color: 'white' }}
@@ -42,7 +43,7 @@ const IMSPage = () => {
             // Add link to Order Management page
             component={Link}
             to="/order-management"
-            fullWidth
+            fullWidth={isSmallScreen}
           >
             Order Management
           </Button>
@@ -54,7 +55,7 @@ const IMSPage = () => {
             // Add link to Supplier Management page
             component={Link}
             to="/supplier-management"
-            fullWidth
+            fullWidth={isSmallScreen}
           >
             Supplier Management
           </Button>
@@ -66,7 +67,7 @@ const IMSPage = () => {
             // Add link to Reports and Analytics page
             component={Link}
             to="/reports-analytics"
-            fullWidth
+            fullWidth={isSmallScreen}
           >
             Reports and Analytics
           </Button>
@@ -77,7 +78,7 @@ const IMSPage = () => {
             startIcon={<Icon icon="clarity:alert-line" />}
             // Add link or handle Low Stock Alerts
             onClick={handleLowStockAlerts}
-            fullWidth
+            fullWidth={isSmallScreen}
           >
             Low Stock Alerts
           </Button>
@@ -88,7 +89,7 @@ const IMSPage = () => {
             startIcon={<Icon icon="fluent:alert-28-filled" />}
             // Add link or handle Order Status Updates
             onClick={handleOrderStatusUpdates}
-            fullWidth
+            fullWidth={isSmallScreen}
           >
             Order Status Updates
           </Button>
@@ -102,87 +103,7 @@ const IMSPage = () => {
       </Scrollbar>
     </Container>
   );
-  // return (
-  //   <Container>
-  //     <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
-  //       <Typography variant="h4">Inventory Management System</Typography>
-
-  //       {/* Add buttons that link to vital elements of IMS */}
-  //       <Link to="/new-inventory-item">
-  //         <Button
-  //           variant="contained"
-  //           style={{ backgroundColor: '#0096FF', color: 'white' }}
-  //         >
-  //           New Inventory Item
-  //         </Button>
-  //       </Link>
-
-  //       {/* Add more buttons as needed */}
-  //     </Stack>
-
-  //     {/* Add buttons or icons for key elements */}
-  //     <Stack direction="row" spacing={2} mb={3}>
-  //       <Button
-  //         variant="contained"
-  //         style={{ backgroundColor: '#00CC66', color: 'white' }}
-  //         startIcon={<Icon icon="vaadin:truck" />}
-  //         // Add link to Order Management page
-  //         component={Link}
-  //         to="/order-management"
-  //       >
-  //         Order Management
-  //       </Button>
-
-  //       <Button
-  //         variant="contained"
-  //         style={{ backgroundColor: '#FF9900', color: 'white' }}
-  //         startIcon={<Icon icon="mdi:people-outline" />}
-  //         // Add link to Supplier Management page
-  //         component={Link}
-  //         to="/supplier-management"
-  //       >
-  //         Supplier Management
-  //       </Button>
-
-  //       <Button
-  //         variant="contained"
-  //         style={{ backgroundColor: '#FF3366', color: 'white' }}
-  //         startIcon={<Icon icon="fxemoji:barchart" />}
-  //         // Add link to Reports and Analytics page
-  //         component={Link}
-  //         to="/reports-analytics"
-  //       >
-  //         Reports and Analytics
-  //       </Button>
-
-  //       <Button
-  //         variant="contained"
-  //         style={{ backgroundColor: '#FF3366', color: 'white' }}
-  //         startIcon={<Icon icon="clarity:alert-line" />}
-  //         // Add link or handle Low Stock Alerts
-  //         onClick={() => handleLowStockAlerts()}
-  //       >
-  //         Low Stock Alerts
-  //       </Button>
-
-  //       <Button
-  //         variant="contained"
-  //         style={{ backgroundColor: '#FF3366', color: 'white' }}
-  //         startIcon={<Icon icon="fluent:alert-28-filled" />}
-  //         // Add link or handle Order Status Updates
-  //         onClick={() => handleOrderStatusUpdates()}
-  //       >
-  //         Order Status Updates
-  //       </Button>
-  //     </Stack>
-
-  //     {/* Display the current inventory table */}
-  //     <Scrollbar>
-  //       {/* <InventoryTable /> */}
-  //       INVENTORY TABLE HERE
-  //     </Scrollbar>
-  //   </Container>
-  // );
 };
 
 export default IMSPage;
+
