3ef2c551d08234b20c8e1ff1f2ba0e06401bc51e
blackgenius007 <truthin3dimension@gmail.com>
2023-12-10 05:27:22 +0100

    update inventory

---
+++ material-kit-react-main/src/sections/IMS/view/dump.jsx
@@ -0,0 +1,485 @@
+/* eslint-disable */
+import React, { useState, useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
+import { useDownloadExcel } from 'react-export-table-to-excel';
+import Scrollbar from 'src/components/scrollbar';
+import { getAllInventory } from 'src/Services/ProcureServices/inventorySlice';
+
+import { Clear, FileCopy } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+
+import Dialog from '@mui/material/Dialog';
+
+import DialogTitle from '@mui/material/DialogTitle';
+
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import { InputAdornment } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+
+import PropTypes from 'prop-types';
+
+import { useNavigate } from 'react-router-dom';
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
+}));
+
+// Add futuristic styles here...
+const futuristicStyles = {
+  tableContainer: {
+    background: '#222',
+    borderRadius: '10px',
+    padding: '1rem',
+  },
+  table: {
+    color: '#fff',
+    // width: '100%',
+    // borderCollapse: 'collapse',
+    textAlign: 'left',
+    borderCollapse: 'separate', // Separate borders for cells
+    borderSpacing: '0', // No spacing between cells
+  },
+  tableHead: {
+    background: '#333',
+  },
+  tableHeadCell: {
+    padding: '0.5rem',
+    textAlign: 'center', // Align header cells to center
+  },
+  tableBodyRow: {
+    borderBottom: '1px solid #444',
+  },
+  tableBodyCell: {
+    // padding: '0.5rem',
+    // textAlign: 'center', // Align body cells to center
+    border: '1px solid #444', // Add border to each cell
+    padding: '0.5rem',
+    textAlign: 'center',
+  },
+  avatar: {
+    borderRadius: '50%',
+    width: '30px',
+    height: '30px',
+  },
+  link: {
+    background: 'none',
+    border: 'none',
+    color: '#00aaff',
+    cursor: 'pointer',
+    textDecoration: 'none',
+    fontSize: '14px',
+    fontFamily: 'inherit',
+    padding: 0,
+    margin: 0,
+  },
+  actionIcons: {
+    fontSize: '1.1rem',
+    color: '#fff',
+    marginRight: '0.5rem',
+    cursor: 'pointer',
+    transition: 'color 0.3s ease-in-out',
+  },
+};
+
+function BootstrapDialogTitle(props) {
+  const { children, onClose, ...other } = props;
+
+  return (
+    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+      {children}
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </DialogTitle>
+  );
+}
+
+BootstrapDialogTitle.propTypes = {
+  children: PropTypes.node,
+  onClose: PropTypes.func.isRequired,
+};
+
+const InventoryTable = ({ drawer }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [dateOffset, setDateOffset] = useState(7);
+  const [searched, setSearched] = useState('');
+  const [add, setAdd] = useState(0);
+  const [input, setInput] = useState(0);
+  const [minus, setMinus] = useState(0);
+  const [rowInputValues, setRowInputValues] = useState({});
+  const [open, setOpen] = useState(false);
+  const [exportMode, setExportMode] = useState(0);
+
+  const [filteredRows, setFilteredRows] = useState([]);
+  let totalRemunerationForAll = 0;
+  // Pagination state
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  //user details
+  const { role, email, ownerEmail } = useSelector((state) => state.auth.user.data);
+  // user role
+  const userEmail = role === 'owner' || role === 'admin' ? ownerEmail : email;
+  console.log(userEmail);
+
+  //fetch attendance details
+  useEffect(() => {
+    dispatch(getAllInventory({ userEmail, dateOffset }));
+  }, [dispatch, userEmail, dateOffset]);
+
+  // Retrieve inventory data from inventorySlice
+  const { inventory, isLoading, isError, message } = useSelector((state) => state.inventory);
+  console.log('all inventory:', inventory);
+  // search function
+  const requestSearch = (searchedVal) => {
+    setSearched(searchedVal);
+  };
+
+  // Function to enable export mode
+  const enableExportMode = () => {
+    setExportMode(true);
+  };
+
+  // Function to disable export mode
+  const disableExportMode = () => {
+    setExportMode(false);
+  };
+  const ExportSheet = () => {
+    setExportMode(1);
+  };
+
+  const handleAddChange = (id, quantity) => {
+    console.log('=================', id);
+    var nums = parseInt(quantity) + parseInt(add);
+    console.log(nums);
+  };
+
+  const handleSubChange = (id, quantity) => {
+    if (parseInt(quantity) - parseInt(minus) < 0) {
+      alert('The input value is too high.');
+    } else {
+      var nums = parseInt(quantity) - parseInt(minus);
+      const order = prompt('Please enter destination of item');
+
+      console.log(nums, order);
+    }
+  };
+
+  const totalPages = Math.ceil(filteredRows.length / rowsPerPage);
+  const startIndex = page * rowsPerPage;
+  const endIndex = startIndex + rowsPerPage;
+  const paginatedRows = filteredRows.slice(startIndex, endIndex);
+
+  // convert currency
+  const toMoney = (value) => {
+    return accounting.formatMoney(accounting.unformat(value), {
+      symbol: '',
+      precision: 2,
+    });
+  };
+
+  // Function to handle 'add' input change for a specific row
+  const handleAddInputChange = (rowId, value) => {
+    // Create a copy of the current rowInputValues object
+    const updatedRowInputValues = { ...rowInputValues };
+    // Update the 'add' value for the specific row
+    updatedRowInputValues[rowId] = { ...updatedRowInputValues[rowId], add: value };
+    setRowInputValues(updatedRowInputValues);
+  };
+
+  // Function to handle 'minus' input change for a specific row
+  const handleMinusInputChange = (rowId, value) => {
+    // Create a copy of the current rowInputValues object
+    const updatedRowInputValues = { ...rowInputValues };
+    // Update the 'minus' value for the specific row
+    updatedRowInputValues[rowId] = { ...updatedRowInputValues[rowId], minus: value };
+    setRowInputValues(updatedRowInputValues);
+  };
+
+  // Dialogue box
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const tableRef = useRef(null);
+
+  // excel download function
+  const { onDownload } = useDownloadExcel({
+    currentTableRef: tableRef.current,
+    filename: 'Salary',
+    sheet: 'Users',
+  });
+
+  return (
+    <>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <TextField
+          value={searched}
+          onChange={(e) => requestSearch(e.target.value)}
+          label="Search database"
+          variant="outlined"
+          size="small"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                {searched && (
+                  <IconButton onClick={() => requestSearch('')}>
+                    <Clear />
+                  </IconButton>
+                )}
+              </InputAdornment>
+            ),
+          }}
+        />
+      </div>
+      <br />
+
+      <div style={futuristicStyles.tableContainer}>
+        <table style={futuristicStyles.table}>
+          <thead style={futuristicStyles.tableHead}>
+            <tr>
+              <th style={futuristicStyles.tableHeadCell}>Item image</th>
+              <th style={futuristicStyles.tableHeadCell}>Item</th>
+              <th style={futuristicStyles.tableHeadCell}>Category</th>
+              <th style={futuristicStyles.tableHeadCell}>Location</th>
+              <th style={futuristicStyles.tableHeadCell}>Description</th>
+              <th style={futuristicStyles.tableHeadCell}>Supplier</th>
+              <th style={futuristicStyles.tableHeadCell}>Price</th>
+              <th style={futuristicStyles.tableHeadCell}>Quantity</th>
+              <th style={futuristicStyles.tableHeadCell}>Modified</th>
+              <th style={futuristicStyles.tableHeadCell}>Re-stock</th>
+              <th style={futuristicStyles.tableHeadCell}>Out-going</th>
+            </tr>
+          </thead>
+          <tbody>
+            {paginatedRows.map((row) => {
+              console.log('in-map:', row);
+              const { _id, employeeName, designation } = row;
+              const { grossIncome, country } = designation;
+              const employeeFinancialData = financialData[_id];
+
+              // Calculate financial data if not available
+              if (!employeeFinancialData) {
+                calculateFinancialData(_id, grossIncome, country);
+                return null; // Render nothing for now, will be updated on next render
+              }
+              console.log(employeeFinancialData);
+              // Calculate financial data if not available
+              if (!employeeFinancialData) {
+                calculateFinancialData(_id, grossIncome, country);
+                return null; // Render nothing for now, will be updated on next render
+              }
+              console.log(employeeFinancialData);
+
+              // Calculate Total Remuneration
+              const monthlyRate = employeeFinancialData.monthlySalary;
+              const totalRemuneration = (
+                monthlyRate +
+                row.overtime +
+                row.allowance -
+                row.IOU
+              ).toFixed(2);
+
+              // Calculate net Remuneration
+              const netRemuneration =
+                monthlyRate +
+                parseFloat(row.overtime) +
+                parseFloat(row.allowance) -
+                parseFloat(row.IOU);
+              // Add the calculated remuneration to the total
+              totalRemunerationForAll += netRemuneration;
+              console.log('All:', totalRemunerationForAll);
+              return (
+                <tr key={row.id} style={futuristicStyles.tableBodyRow}>
+                  {exportMode === 1 ? (
+                    ''
+                  ) : (
+                    <td style={futuristicStyles.tableBodyCell}>
+                      <img src={row.imagePath} alt="" style={futuristicStyles.avatar} />
+                    </td>
+                  )}
+
+                  <td style={futuristicStyles.tableBodyRow}>
+                    <Link
+                      to={`/employee-detail/${row._id}`}
+                      style={{ textDecoration: 'none', color: 'white' }}
+                    >
+                      {' '}
+                      {employeeName}
+                    </Link>{' '}
+                  </td>
+
+                  <td style={futuristicStyles.tableBodyCell}>
+                    <Link
+                      to={`/employee-detail/${row._id}`}
+                      style={{
+                        color: '#ffff',
+                        textDecoration: 'none',
+                        backgroundImage: 'none',
+                      }}
+                    >
+                      {row.designation.designation}
+                    </Link>
+                  </td>
+
+                  <td style={futuristicStyles.tableBodyCell}>
+                    {toMoney(employeeFinancialData.monthlySalary)}
+                  </td>
+
+                  <td style={futuristicStyles.tableBodyCell}>
+                    {toMoney(row.allowance ? row.allowance : '0.00')}
+                  </td>
+                  <td style={futuristicStyles.tableBodyCell}>{toMoney(row.overtime)}</td>
+                  <td style={futuristicStyles.tableBodyCell}>{toMoney(row.IOU)}</td>
+                  <td style={futuristicStyles.tableBodyCell}>{toMoney(row.location)}</td>
+                  <td style={futuristicStyles.tableBodyCell}>{toMoney(row.I)}</td>
+
+                  <td style={futuristicStyles.tableBodyCell}>
+                    <input
+                      type="number"
+                      style={{
+                        width: '50px',
+                        padding: '5px',
+                        border: 'none',
+                        borderBottom: '1px solid #ddd',
+                        background: 'transparent',
+                        color: 'white',
+                      }}
+                      value={rowInputValues[row._id]?.add || ''}
+                      onChange={(e) => handleAddInputChange(row._id, e.target.value)}
+                    />
+                    <IconButton
+                      onClick={() => handleAddChange(row._id, row.quantity)}
+                      style={{ color: '#26a69a' }}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="icon icon-tabler icon-tabler-plus"
+                        width="15"
+                        height="15"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="#26a69a"
+                        fill="none"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      >
+                        +
+                      </svg>
+                    </IconButton>
+                  </td>
+                  <td style={futuristicStyles.tableBodyCell}>
+                    <input
+                      type="number"
+                      style={{
+                        width: '50px',
+                        padding: '5px',
+                        border: 'none',
+                        borderBottom: '1px solid #ddd',
+                        background: 'transparent',
+                        color: 'white',
+                      }}
+                      value={rowInputValues[row._id]?.minus || ''}
+                      onChange={(e) => handleMinusInputChange(row._id, e.target.value)}
+                    />
+                    <IconButton
+                      onClick={() => handleSubChange(row._id, row.quantity)}
+                      style={{ color: '#e57373' }}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="icon icon-tabler icon-tabler-transfer-out"
+                        width="15"
+                        height="15"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="#e57373"
+                        fill="none"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      >
+                        -
+                      </svg>
+                    </IconButton>
+                  </td>
+                </tr>
+              );
+            })}
+
+            <tr>
+              <td colSpan="8" style={{ textAlign: 'right' }}>
+                <strong>Total:</strong>
+              </td>
+              <td style={{ textAlign: 'center' }}>
+                {/* {toMoney(totalRemunerationForAll.toFixed(2)/2) } */}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <br />
+      {/* Pagination controls */}
+      <div>
+        <button onClick={() => setPage(Math.max(0, page - 1))} disabled={page === 0}>
+          Previous
+        </button>
+        <span>
+          Page {page + 1} of {totalPages}
+        </span>
+        <button
+          onClick={() => setPage(Math.min(totalPages - 1, page + 1))}
+          disabled={page === totalPages - 1}
+        >
+          Next
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default function MainPage() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <Container>
+        
+      <Scrollbar>
+        <InventoryTable drawer={toggleSidebar} />
+      </Scrollbar>
+    </Container>
+  );
+}
