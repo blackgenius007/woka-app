b73db76d1cf2459b19bf29c303bf5316d5f3653f
blackgenius007 <truthin3dimension@gmail.com>
2023-12-12 06:49:34 +0100

    update inventory

--- material-kit-react-main/src/components/Home/hero.jsx
+++ material-kit-react-main/src/components/Home/hero.jsx
@@ -39,7 +39,7 @@
         >
           <div className="landing-text" style={{ flex: 1, minWidth: '465px' }}>
             <h1 style={{ width: '100%', fontSize: '3.9em', lineHeight: '1.15' }}>
-              Seamless efficiency, all-in-one solution.
+              Seamless efficiency,all-in-one solution.
             </h1>
             <p style={{ fontSize: '18px', maxWidth: '500px', margin: '0 0 29.41176px' }}>
               Streamline employee management, innovate inventory, breeze through procurement. Elevate
