230fe3703f5429788de638debd0f919f45c584e8
blackgenius007 <truthin3dimension@gmail.com>
2023-12-12 09:00:53 +0100

    update inventory

--- material-kit-react-main/src/components/Home/hero.jsx
+++ material-kit-react-main/src/components/Home/hero.jsx
@@ -1,7 +1,7 @@
   /* eslint-disable */
  /* eslint-disable */
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
+import { Typography } from '@mui/material';
 
 function Hero() {
   return (
