a859f09552471e2544f09856c16f4f96486ae671
blackgenius007 <truthin3dimension@gmail.com>
2023-12-12 23:55:54 +0100

    update home page

--- material-kit-react-main/src/Services/ProcureServices/inventoryServices.js
+++ material-kit-react-main/src/Services/ProcureServices/inventoryServices.js
@@ -22,9 +22,10 @@ const API_URL_DELETE_INVENTORY = '/api/v1/inventory/delete';
 const constructURL = (endpoint) => `${BASE_URL}${endpoint}`;
 
 // Get all inventory items
-const getAllInventory = async (email, projectName) => {
-//   const response = await axios.get(constructURL(`${API_URL_ALL_INVENTORY}/${email}/${projectName}`));
-const response = await axios.get(constructURL(`${API_URL_ALL_INVENTORY}/${email} `));
+const getAllInventory = async (email) => {
+    console.log('inventory-services',email)
+  //   const response = await axios.get(constructURL(`${API_URL_ALL_INVENTORY}/${email}/${projectName}`));
+  const response = await axios.get(constructURL(`${API_URL_ALL_INVENTORY}/${email} `));
   return response.data;
 };
 
@@ -36,7 +37,10 @@ const getOneInventory = async (id) => {
 
 // Create a new inventory item
 const createInventory = async (id, projectName, inventoryData) => {
-  const response = await axios.post(constructURL(`${API_URL_CREATE_INVENTORY}/${id}/${projectName}`), inventoryData);
+  const response = await axios.post(
+    constructURL(`${API_URL_CREATE_INVENTORY}/${id}/${projectName}`),
+    inventoryData
+  );
   return response.data;
 };
 
@@ -48,7 +52,9 @@ const quantityPerItem = async (email, projectName) => {
 
 // Get total inventory count
 const getTotalInventory = async (email, projectName) => {
-  const response = await axios.get(constructURL(`${API_URL_TOTAL_INVENTORY}/${email}/${projectName}`));
+  const response = await axios.get(
+    constructURL(`${API_URL_TOTAL_INVENTORY}/${email}/${projectName}`)
+  );
   return response.data;
 };
 
@@ -60,25 +66,34 @@ const getTotalCost = async (email, projectName) => {
 
 // Outgoing stock
 const outGoingStock = async (email, id, num, quantity, order) => {
-  const response = await axios.get(constructURL(`${API_URL_OUTGOING_STOCK}/${email}/${id}/${num}/${quantity}/${order}`));
+  const response = await axios.get(
+    constructURL(`${API_URL_OUTGOING_STOCK}/${email}/${id}/${num}/${quantity}/${order}`)
+  );
   return response.data;
 };
 
 // Incoming stock
 const incomingStock = async (email, id, num, quantity) => {
-  const response = await axios.get(constructURL(`${API_URL_INCOMING_STOCK}/${email}/${id}/${num}/${quantity}`));
+  const response = await axios.get(
+    constructURL(`${API_URL_INCOMING_STOCK}/${email}/${id}/${num}/${quantity}`)
+  );
   return response.data;
 };
 
 // Get stock balance
 const getStockBalance = async (email, projectName) => {
-  const response = await axios.get(constructURL(`${API_URL_STOCK_BALANCE}/${email}/${projectName}`));
+  const response = await axios.get(
+    constructURL(`${API_URL_STOCK_BALANCE}/${email}/${projectName}`)
+  );
   return response.data;
 };
 
 // Update inventory item
 const updateInventory = async (id, inventoryData) => {
-  const response = await axios.post(constructURL(`${API_URL_UPDATE_INVENTORY}/${id}`), inventoryData);
+  const response = await axios.post(
+    constructURL(`${API_URL_UPDATE_INVENTORY}/${id}`),
+    inventoryData
+  );
   return response.data;
 };
 
