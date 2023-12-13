35cfb29523288f72d3289fb7c49537cf2d74de86
blackgenius007 <truthin3dimension@gmail.com>
2023-12-13 13:11:17 +0100

    update inventory

--- server/utils/generateCode.js
+++ server/utils/generateCode.js
@@ -1,16 +1,40 @@
-const GenerateCode = (n) => {
-  var add = 1,
-    max = 2 + add; // Update the max value to 2 for the year part
+const generateCode = (n) => {
+  const add = 1;
+  let max = 2 + add; // Update the max value to 2 for the year part
 
   if (n > max) {
-    return generate(max) + generate(n - max);
+    return Math.random().toString().slice(2, n + 2);
   }
 
   max = Math.pow(10, n + add);
-  var min = max / 10; // Math.pow(10, n) basically
-  var number = Math.floor(Math.random() * (max - min + 1)) + min;
+  const min = max / 10; // Math.pow(10, n) basically
+  const number = Math.floor(Math.random() * (max - min + 1)) + min;
 
-  return ("" + number).substring(add);
+  return String(number).slice(add);
 };
 
-module.exports = GenerateCode;
+module.exports = generateCode;
+
+
+
+
+
+
+
+
+// const GenerateCode = (n) => {
+//   var add = 1,
+//     max = 2 + add; // Update the max value to 2 for the year part
+
+//   if (n > max) {
+//     return generate(max) + generate(n - max);
+//   }
+
+//   max = Math.pow(10, n + add);
+//   var min = max / 10; // Math.pow(10, n) basically
+//   var number = Math.floor(Math.random() * (max - min + 1)) + min;
+
+//   return ("" + number).substring(add);
+// };
+
+// module.exports = GenerateCode;
