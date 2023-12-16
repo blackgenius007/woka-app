4f4392a2ac84c01c6845398a1b52007b6d0c1e71
blackgenius007 <truthin3dimension@gmail.com>
2023-12-16 09:00:35 +0100

    portal

--- material-kit-react-main/src/sections/portalAccess/view/portal-main.jsx
+++ material-kit-react-main/src/sections/portalAccess/view/portal-main.jsx
@@ -49,33 +49,7 @@ const handleClose=()=>{
   return (
     <>
   <PortalBody />
-     <Typography
-            style={{  marginLeft: '10px',color:'#A9A9A9'}}
-            className='panelText'
-            variant="h4"
-           
-          >
-            Don't have a Job seeker account? <Button onClick={handleOpen}><Typography  style={{ textDecoration: 'none', color:'orange'}} variant='h5'>Sign up</Typography></Button>
-          </Typography>
-      <br />
-      <Grid container>
-                <Grid md={8}>
-
-                
-          {/* <div>
-            <Image style={{ maxHeight: '200px' }} src={filyIcon3} rounded />
-          </div> */}
-        </Grid>
-        <Grid md={4}>
-        
-          {/* <Carousel/> */}
-        
-         
-          
-        </Grid>
-      </Grid>
-
-      {/* <Register open={open} handleClose={handleClose}/> */}
+  
     </>
   );
 }
