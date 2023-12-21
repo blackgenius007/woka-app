a334a7d5163cb7088fe669836c23dfb2295c091f
blackgenius007 <truthin3dimension@gmail.com>
2023-12-21 14:23:55 +0100

    portal payroll

--- material-kit-react-main/src/sections/portalAccess/view/portal-body.jsx
+++ material-kit-react-main/src/sections/portalAccess/view/portal-body.jsx
@@ -429,27 +429,29 @@ const EmployeePortal = () => {
             </Popover>
             {/* Tax Calculator Popover */}
             <Popover
-              open={taxCalculatorPopoverOpen}
-              anchorEl={anchorEl}
-              onClose={handleTaxCalculatorPopoverClose}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'center',
-              }}
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'center',
-              }}
-            >
-              <TaxCalculator
-                cra={cra}
-                grossIncome={grossIncome}
-                healthCare={healthCare}
-                loan={loan}
-                benefit={benefitInKind}
-                pensionFund={pensionFund}
-              />
-            </Popover>
+  open={taxCalculatorPopoverOpen}
+  anchorEl={null} // Set anchorEl to null
+  anchorReference="anchorPosition" // Use anchorPosition for centering
+  anchorPosition={{ top: '50%', left: '50%' }} // Center of the page
+  onClose={handleTaxCalculatorPopoverClose}
+  anchorOrigin={{
+    vertical: 'center',
+    horizontal: 'center',
+  }}
+  transformOrigin={{
+    vertical: 'center',
+    horizontal: 'center',
+  }}
+>
+  <TaxCalculator
+    cra={cra}
+    grossIncome={grossIncome}
+    healthCare={healthCare}
+    loan={loan}
+    benefit={benefitInKind}
+    pensionFund={pensionFund}
+  />
+</Popover>
           </>
         ))}
     </>
