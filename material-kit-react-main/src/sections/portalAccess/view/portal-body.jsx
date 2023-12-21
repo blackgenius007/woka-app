4937ff6fa7974c1137e82e59790096cb4b70ed2d
blackgenius007 <truthin3dimension@gmail.com>
2023-12-21 15:44:44 +0100

    portal payroll

--- material-kit-react-main/src/sections/portalAccess/view/portal-body.jsx
+++ material-kit-react-main/src/sections/portalAccess/view/portal-body.jsx
@@ -411,7 +411,7 @@ const EmployeePortal = () => {
             >
               <div style={{ padding: '16px' }}>
                 <Typography variant="h6" gutterBottom>
-                  {businessName} Data Collection Point
+                  {employeeAccessData.businessName} Data Collection Point
                 </Typography>
 
                 <div style={{ display: 'flex', alignItems: 'center', marginBottom: '8px' }}>
