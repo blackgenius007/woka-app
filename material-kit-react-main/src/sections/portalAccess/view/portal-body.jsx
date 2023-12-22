2d24c0ad7d7ce80911b613f3ec47687ea7264461
blackgenius007 <truthin3dimension@gmail.com>
2023-12-22 18:59:00 +0100

    portal inventory

--- material-kit-react-main/src/sections/portalAccess/view/portal-body.jsx
+++ material-kit-react-main/src/sections/portalAccess/view/portal-body.jsx
@@ -41,22 +41,8 @@ import DataIcon from '@mui/icons-material/DataUsage';
 const EmployeePortal = () => {
   const dispatch = useDispatch();
   const { employees,DataAuth, isLoading } = useSelector((state) => state.employees);
- // Find the item in DataAuth that matches the portalCode
-const relevantDataAuthItem = DataAuth.find((item) =>
-item.collectionPointDetails.some((detail) => detail.employeeNumber === portalCode)
-);
-
-// Extract collectionPointDetails from the relevant item
-const collectionPointDetails = relevantDataAuthItem?.collectionPointDetails || [];
-
-console.log('portalCode:', portalCode);
-console.log('DataAuth:', DataAuth);
-console.log('relevantDataAuthItem:', relevantDataAuthItem);
-console.log('collectionPointDetails:', collectionPointDetails);
-
-// Now you can use collectionPointDetails in your component
-console.log(collectionPointDetails);
-  console.log(employees,DataAuth);
+  
+  console.log(employees,DataAuth.collectionPointDetail);
   const [isEditing, setIsEditing] = useState(false);
   const [employeeData, setEmployeeData] = useState(null);
   const [employeeAccessData, setEmployeeAccessData] = useState([]);
