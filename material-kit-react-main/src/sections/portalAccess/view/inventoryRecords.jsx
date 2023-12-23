a9844e74d7fd6fd5372577204ac0752a36a73609
blackgenius007 <truthin3dimension@gmail.com>
2023-12-23 06:18:05 +0100

    portal inventory

--- material-kit-react-main/src/sections/portalAccess/view/inventoryRecords.jsx
+++ material-kit-react-main/src/sections/portalAccess/view/inventoryRecords.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
@@ -12,36 +11,21 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import CloseIcon from '@mui/icons-material/Close';
 import Slide from '@mui/material/Slide';
-import{getAllInventoryEachPoint} from 'src/Services/ProcureServices/inventorySlice'
+import { getAllInventoryEachPoint } from 'src/Services/ProcureServices/inventorySlice';
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
-export default function InventoryDialog({open, close,employeeNumber, tag ,email}) {
- 
-
- console.log('inventory:',employeeNumber,tag ,email)
-
- 
+export default function InventoryDialog({ open, close, employeeNumber, tag, email }) {
+  console.log('inventory:', employeeNumber, tag, email);
 
   return (
     <React.Fragment>
-    
-      <Dialog
-        fullScreen
-        open={open}
-        onClose={close}
-        TransitionComponent={Transition}
-      >
-        <AppBar sx={{ position: 'relative' }}>
+      <Dialog fullScreen open={open} onClose={close} TransitionComponent={Transition}>
+        <AppBar sx={{ position: 'relative', backgroundColor: '#fff', color: '#000' }}>
           <Toolbar>
-            <IconButton
-              edge="start"
-              color="inherit"
-              onClick={close}
-              aria-label="close"
-            >
+            <IconButton edge="start" color="inherit" onClick={close} aria-label="close">
               <CloseIcon />
             </IconButton>
             <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
@@ -58,10 +42,7 @@ export default function InventoryDialog({open, close,employeeNumber, tag ,email}
           </ListItem>
           <Divider />
           <ListItem button>
-            <ListItemText
-              primary="Default notification ringtone"
-              secondary="Tethys"
-            />
+            <ListItemText primary="Default notification ringtone" secondary="Tethys" />
           </ListItem>
         </List>
       </Dialog>
