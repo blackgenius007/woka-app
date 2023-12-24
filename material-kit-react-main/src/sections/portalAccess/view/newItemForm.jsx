5b2eb12508798d57af0500fee6ac8a18411c3c84
blackgenius007 <truthin3dimension@gmail.com>
2023-12-24 19:45:38 +0100

    inventory

--- material-kit-react-main/src/sections/portalAccess/view/newItemForm.jsx
+++ material-kit-react-main/src/sections/portalAccess/view/newItemForm.jsx
@@ -3,109 +3,58 @@ import React, { useState, useEffect, Fragment } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
  import ItemStatus from './itemStatus';
 import { Grid, Button, TextField, Typography, MenuItem } from '@mui/material';
-// import { registerInmate } from '../InmateServices/inmateSlice';
- 
+  
 
 const containerStyle = {
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
+  justifyContent: 'center', // Center horizontally
   padding: '16px',
   overflowX: 'hidden',
-};
-
-const formStyle = {
-  width: '100%',
 };
 
 const columnStyle = {
   display: 'flex',
   flexDirection: 'column',
   gap: '8px',
-};
-
-const smallInputStyle = {
-  width: '200px',
+  width: '100%', // Set column width to 100%
 };
 
 const NewItem = () => {
   const [formData, setFormData] = useState({
-    inmate_name: '',
-    date_of_birth: '',
-    gender: '',
-    socialSecurityNumber: ' ',
-    raceEthnicity: '',
-    nationality: '',
-    homeAddress: ' ',
-    height: '',
-    weight: '',
-    eye_color: '',
-    hair_color: '',
-    distinguishingMarks: '',
-    disabilities: '',
-    bookingDateTime: '',
-    bookingNumber: '',
-    bookingOfficer: '',
-    arrestingAgency: '',
-    arrestingOfficer: '',
-    offence_category: '',
-    arrestDate: '',
-    arrestLocation: '',
-    arrestJurisdiction: '',
-    personalItems: '',
-    contactInformation: '',
+    itemName: '',
+    description: '',
+    SKU: '',
+    price: '',
+    stock: '',
+    category: '',
+    supplier: '',
+    status: '',
   });
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-
-    // Handle special case for date inputs
-    if (
-      event.target.type === 'date' ||
-      event.target.type === 'datetime-local'
-    ) {
-      setFormData({
-        ...formData,
-        [name]: value, // Update directly for date inputs
-      });
-    } else {
-      setFormData({
-        ...formData,
-        [name]: value,
-      });
-    }
-  };
-
- 
-  const handleItemStatusChange = (event) => {
     setFormData({
       ...formData,
-      ItemStatus: event.target.value,
+      [name]: value,
     });
   };
 
-    // const { user } = useSelector((state) => state.auth);
- 
-
-//   const dispatch = useDispatch();
-
-//     const handleSubmit = (values) => {
-//     const inmateId = user.data._id;
-//     // Handle form submission
-//     console.log(user.data._id);
-//     // Dispatch registerInmate action
-//     dispatch(registerInmate({ inmateData: formData, inmateId }));
-//     alert('submit successful!!')
-//   };
-  
+  const handleItemStatusChange = (event) => {
+    setFormData({
+      ...formData,
+      status: event.target.value,
+    });
+  };
 
   return (
     <div style={containerStyle}>
       <Typography variant="h5" component="h1" style={{ color: 'grey' }}>
         NEW ITEM FORM
       </Typography>
-      <br/>
-      <form  style={formStyle}>
+      <br />
+      <form>
         <Grid container spacing={2}>
           <Grid item xs={12} sm={4} style={columnStyle}>
             <TextField
@@ -117,7 +66,7 @@ const NewItem = () => {
               value={formData.itemName}
               onChange={handleInputChange}
             />
-               <TextField
+            <TextField
               variant="outlined"
               size="small"
               fullWidth
@@ -126,7 +75,7 @@ const NewItem = () => {
               value={formData.description}
               onChange={handleInputChange}
             />
-             <TextField
+            <TextField
               variant="outlined"
               size="small"
               fullWidth
@@ -135,22 +84,18 @@ const NewItem = () => {
               value={formData.SKU}
               onChange={handleInputChange}
             />
-          
-         
             <TextField
               variant="outlined"
               size="small"
               fullWidth
               label="Unit Price"
               name="price"
-              value={formData.price }
+              value={formData.price}
               onChange={handleInputChange}
             />
-
-            
           </Grid>
           <Grid item xs={12} sm={4} style={columnStyle}>
-          <TextField
+            <TextField
               variant="outlined"
               size="small"
               fullWidth
@@ -159,7 +104,7 @@ const NewItem = () => {
               value={formData.stock}
               onChange={handleInputChange}
             />
-          <TextField
+            <TextField
               variant="outlined"
               size="small"
               fullWidth
@@ -177,9 +122,7 @@ const NewItem = () => {
               value={formData.supplier}
               onChange={handleInputChange}
             />
-  
-             
-  <TextField
+            <TextField
               select
               label="Item Status"
               name="status"
@@ -194,10 +137,7 @@ const NewItem = () => {
                 </MenuItem>
               ))}
             </TextField>
-
-        
           </Grid>
-         
         </Grid>
         <Button
           type="submit"
@@ -213,148 +153,3 @@ const NewItem = () => {
 };
 
 export default NewItem;
-
-
-
-
-
-
-// import React, { useState, Fragment } from 'react';
-// import { useSelector } from 'react-redux';
-// import Button from '@mui/material/Button';
-// import TextField from '@mui/material/TextField';
-
-// export default function NewInventoryForm() {
-//   const [inventory, setInventory] = useState({
-//     itemName: '',
-//     description: '',
-//     price: '',
-//     quantity: '',
-//     itemStatus: '',
-//   });
-
-//   function handleChange(e) {
-//     const { name, value } = e.target;
-//     setInventory((inventory) => ({ ...inventory, [name]: value }));
-//   }
-
-// //   const SubmitHandle = (e) => {
-// //     e.preventDefault();
-
-// //     if (inventory) {
-// //       axios.post(`/api/v1/inventory/create/${user._id}/${projectname}`, inventory).then(
-// //         (res) => {
-// //           alert('Submitted successfully!');
-// //           handleClose();
-// //         },
-// //         (err) => {
-// //           alert(message, err);
-// //         }
-// //       );
-// //     }
-// //   };
-//   return (
-//     <>
- 
-//         {/* <Popup
-//             trigger={
-//               <IconButton variant="info" color="primary" onClick={handleClickOpen}>
-//                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-forklift" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#9e9e9e" fill="none" stroke-linecap="round" stroke-linejoin="round">
-//      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-//      <circle cx="5" cy="17" r="2" />
-//      <circle cx="14" cy="17" r="2" />
-//      <line x1="7" y1="17" x2="12" y2="17" />
-//      <path d="M3 17v-6h13v6" />
-//      <path d="M5 11v-4h4" />
-//      <path d="M9 11v-6h4l3 6" />   
-//      <path d="M22 15h-3v-10" />
-//      <line x1="16" y1="13" x2="19" y2="13" />
-//   </svg>
-//               </IconButton>
-//             }
-//             position="bottom center"
-//           >
-//             Add new item to store
-//           </Popup> */}
-
-//         <TextField
-//           variant="outlined"
-//           margin="normal"
-//           name="itemName"
-//           value={inventory.itemName}
-//           onChange={handleChange}
-//           required
-//           fullWidth
-//           label="item name"
-//           autoFocus
-//           autoComplete="off"
-//           type="text"
-//         />
-//         <TextField
-//           variant="outlined"
-//           margin="normal"
-//           name="description"
-//           value={inventory.description}
-//           onChange={handleChange}
-//           required
-//           fullWidth
-//           label="description"
-//           autoFocus
-//           autoComplete="off"
-//           type="text"
-//         />
-//         <TextField
-//           variant="outlined"
-//           margin="normal"
-//           name="price"
-//           value={inventory.price}
-//           onChange={handleChange}
-//           required
-//           fullWidth
-//           label="selling price"
-//           autoFocus
-//           autoComplete="off"
-//           type="number"
-//         />
-//         <TextField
-//           variant="outlined"
-//           margin="normal"
-//           name="quantity"
-//           value={inventory.quantity}
-//           onChange={handleChange}
-//           required
-//           fullWidth
-//           label="quantity"
-//           autoFocus
-//           autoComplete="off"
-//           type="number"
-//         />
-//         <TextField
-//           variant="outlined"
-//           margin="normal"
-//           name="itemStatus"
-//           value={inventory.itemStatus}
-//           onChange={handleChange}
-//           required
-//           fullWidth
-//           label="condition of item"
-//           autoFocus
-//           autoComplete="off"
-//           type="text"
-//         />
-
-//         <Button
-//           type="submit"
-//           fullWidth
-//           variant="contained"
-//           color="secondary"
-//         //   disabled={isLoading}
-//           size="large"
-//         >
-//           Submit
-       
-//         </Button>
-   
-//     </>
-//   );
-// }
