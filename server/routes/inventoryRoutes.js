c0f71c2dbf0ece184b4173c7d38a71c48835d04c
blackgenius007 <truthin3dimension@gmail.com>
2023-12-31 01:02:26 +0100

    inventory

--- server/routes/inventoryRoutes.js
+++ server/routes/inventoryRoutes.js
@@ -1,46 +1,83 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 const {
-  createInventoryPool,
-  getAllInventory,
-  getTotalInventory,
-  getOneInventory,
-  updateInventory,
-  deleteInventory,
-  getTotalCost,
-  outGoingStock,
-  incomingStock,
-  getStockBalance,
-  quantityPerItem,
-  connectItem,
-  createInventoryPoolPoint,
-  getAllInventoryEachPoint,
-} = require("../controller/inventory");
+  
+   createInventoryPool,
+   getAllInventory,
+   getTotalInventory,
+   getOneInventory,
+   updateInventory,
+   deleteInventory,
+   getTotalCost,
+   outGoingStock ,
+   incomingStock,
+   getStockBalance,
+   quantityPerItem,
+   connectItem,
+   createInventoryPoolPoint,
+   getAllInventoryEachPoint  
 
-router.route("/:id").get(getOneInventory);
+   
+  } = require('../controller/inventory');
+  
+ 
 
-router.route("/all-inventory/:email").get(getAllInventory);
+  router
+  .route('/:id')
+  .get(getOneInventory);
+ 
+  router
+  .route('/all-inventory/:email')
+  .get(getAllInventory )
 
-router.route("/inventory-point/:email/:tagName").get(getAllInventoryEachPoint);
+  router
+  .route('/inventory-point/:email/:tagName')
+  .get(getAllInventoryEachPoint)
 
-router.route("/create").post(createInventoryPool);
+   
+  router
+  .route('/create')
+  .post(createInventoryPool)
+ 
 
-router.route("/per-item/:email/:projectname").get(quantityPerItem);
+  router
+  .route('/per-item/:email/:projectname')
+  .get(quantityPerItem  )
+ 
+  router
+  .route('/count/:email/:projectname')
+  .get(getTotalInventory )
 
-router.route("/count/:email/:projectname").get(getTotalInventory);
+  router
+  .route('/cost/:email/:projectname')           
+  .get(getTotalCost )
 
-router.route("/cost/:email/:projectname").get(getTotalCost);
+  router
+  .route('/outstock/:email/:id/:num/:quantity/:order')
+  .get( outGoingStock)
 
-router.route("/outstock/:email/:id/:num/:quantity/:order ").get(outGoingStock);
+  router
+  .route('/instock/:email/:id/:nums/:quantity')
+  .get( incomingStock)
 
-router.route("/instock/:email/:id/:nums/:quantity ").get(incomingStock);
+  router
+  .route('/stockbalance/:email/:projectname')
+  .get(getStockBalance)
 
-router.route("/stockbalance/:email/:projectname").get(getStockBalance);
+  router
+  .route('/update/:id')
+  .post(updateInventory )
 
-router.route("/update/:id").post(updateInventory);
+  router
+  .route('/connect')
+  .post(connectItem )
 
-router.route("/connect").post(connectItem);
+  router
+  .route('/delete/:id')
+  .delete(deleteInventory )
 
-router.route("/delete/:id").delete(deleteInventory);
-
-module.exports = router;
+  
+  
+  
+ 
+module.exports = router
